eb4e2f57e24242adf6159d0be826d96f2f72aa08
HmiouiReda <redahmioui0@gmail.com>
2026-09-09 18:32:24 +0200

    corrections in delta computation

--- src/MonteCarlo.hpp
+++ src/MonteCarlo.hpp
@@ -21,6 +21,6 @@ class MonteCarlo{
         MonteCarlo(const MonteCarlo&) = delete;
         MonteCarlo& operator=(const MonteCarlo&) = delete;
         PriceAndStdDev price(const PnlMat* past, double t);
-        void delta(const PnlMat* path,double t,double fdstep, PnlVect* deltas);
+        void delta(const PnlMat* past,double t,double fdstep, PnlVect* deltas, int M);
 
 };
