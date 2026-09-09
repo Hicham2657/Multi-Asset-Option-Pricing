fb28f507b7b8325965fc95b9d1e7c91ef1d035e8
Hicham Ettahri <hicham.ettahri@grenoble-inp.org>
2026-09-09 08:11:14 +0200

    pricer at 0 first try, still needs to be improved

--- src/BlackScholesModel.cpp
+++ src/BlackScholesModel.cpp
@@ -12,20 +12,15 @@ BlackScholesModel::BlackScholesModel(double riskFreeRate, const PnlVect* sigmas,
         throw std::invalid_argument("correlation matrix is not positive definite");
     }
     _G = pnl_vect_new();
-    _S_tildas = pnl_mat_new();
+    _sTilde = pnl_mat_new();
 }
 
 BlackScholesModel::~BlackScholesModel() {
     pnl_vect_free(&_sigmas);
     pnl_mat_free(&_cholesky);
     pnl_vect_free(&_G);
-    pnl_mat_free(&_S_tildas);
+    pnl_mat_free(&_sTilde);
 }
-
-double my_exp(double x)
-{
-    return std::exp(x);
-};
 
 double EPS = 1E-10;
 // helper donné par les profs
@@ -38,43 +33,60 @@ int compute_last_index(double t, double T, int N) {
         return int(t / dt);
     }
 }
-    
-void BlackScholesModel::generateTildePath(int K, int N, PnlRng* rng){
+
+// first step = (t_{i+1} - t) if t not among t_i's
+void BlackScholesModel::simulateSTilde(int K, int N, double first_step, PnlRng* rng){
     int D = getD();
-    pnl_mat_resize(_S_tildas, K, D);
+    pnl_mat_resize(_sTilde, K, D);
     double dt = _timeHorizon/static_cast<double>(N);
-    double sqrt_dt = std::sqrt(dt);
+
     for (int i = 0; i<K; ++i){
         pnl_vect_rng_normal_d(_G, D, rng);
+        double step = (i==0) ? first_step : dt;
+        double sqrt_step = std::sqrt(step);
         for (int d = 0; d<D; ++d){
             PnlVect L_d = pnl_vect_wrap_mat_row(_cholesky, d);
             double sigma_d = GET(_sigmas, d);
-            double drift = (_riskFreeRate - sigma_d*sigma_d/2)* dt;
-            double diffusion = sigma_d*sqrt_dt* pnl_vect_scalar_prod(&L_d, _G);
-            MLET(_S_tildas, i, d) = std::exp(drift + diffusion);
+            double drift = (_riskFreeRate - sigma_d*sigma_d/2)* step;
+            double diffusion = sigma_d*sqrt_step* pnl_vect_scalar_prod(&L_d, _G);
+            MLET(_sTilde, i, d) = std::exp(drift + diffusion);
         }
     }
 }
 
-void BlackScholesModel::buildPath(PnlMat* path, int K){
+void BlackScholesModel::buildPathFromSTilde(PnlMat* path, int K, PnlVect* s_t){
     int N = path->m - 1;
     int D = getD();
-    for (int i = 0; i<K; ++i){
+
+    for (int d = 0; d<D; ++d){
+        MLET(path, N-K+1 ,d) =  GET(s_t, d)* MGET(_sTilde, 0, d);
+    }
+
+    for (int i = 1; i<K; ++i){
         for (int d = 0; d <D; ++d){
-            MLET(path, N-K+i+1, d) = MGET(path, N-K+i, d) * MGET(_S_tildas, i, d); 
+            MLET(path, N-K+i+1, d) = MGET(path, N-K+i, d) * MGET(_sTilde, i, d); 
         }
     }
 }
 
 void BlackScholesModel::asset(const PnlMat *past, double t, PnlMat *path, PnlRng *rng){
     int N = path->m - 1;
-    for (int i = 0; i < past->m; i++){
-        PnlVect past_ith_row = pnl_vect_wrap_mat_row(past, i);
-        pnl_mat_set_row(path, &past_ith_row, i);
+
+    int i = compute_last_index(t, _timeHorizon, N);
+    double dt = _timeHorizon/N;
+
+    for (int l = 0; l <= i; l++){
+        PnlVect past_ith_row = pnl_vect_wrap_mat_row(past, l);
+        pnl_mat_set_row(path, &past_ith_row, l);
     }
-    int K = N + 1 - past->m;
-    generateTildePath(K, N, rng);
-    buildPath(path, K);
+    
+    PnlVect S_t = pnl_vect_wrap_mat_row(past, past->m - 1);
+    double first_step = (std::abs(t - i*dt) < EPS) ? dt : (i+1)*dt - t;
+
+
+    int K = N - i; //why not N+1-i
+    simulateSTilde(K, N, first_step, rng);
+    buildPathFromSTilde(path, K, &S_t);
 }
 
 int BlackScholesModel::getD() const{return _sigmas->size;}
