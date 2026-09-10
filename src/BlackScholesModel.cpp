18b2fc7b3780fb8584b759adfaaf9c14eb0460a1
paul ndong <paul.ndong@grenoble-inp.org>
2026-09-10 11:46:06 +0200

    test a jour

--- src/BlackScholesModel.cpp
+++ src/BlackScholesModel.cpp
@@ -77,7 +77,7 @@ void BlackScholesModel::asset(const PnlMat *past, double t, PnlMat *path, PnlRng
 void BlackScholesModel::shift_asset(double t, PnlMat* path1, PnlMat* path2,double fdstep, int asset_i){
     int N = path1->m-1;
     int i = compute_last_index(t, _timeHorizon, N);
-    for (int j=i+1;j<=N;j++){
+    for (int j=i;j<=N;j++){
         pnl_mat_set(path1, j, asset_i, pnl_mat_get(path1,j, asset_i) * (1.0 + fdstep));
         pnl_mat_set(path2, j, asset_i, pnl_mat_get(path2, j, asset_i) * (1.0 - fdstep));
     }
@@ -85,7 +85,7 @@ void BlackScholesModel::shift_asset(double t, PnlMat* path1, PnlMat* path2,doubl
 void BlackScholesModel::unshift_asset(double t, PnlMat* path1, PnlMat* path2,double fdstep, int asset_i){
     int N = path1->m-1;
     int i = compute_last_index(t, _timeHorizon, N);
-    for (int j=i+1;j<=N;j++){
+    for (int j=i;j<=N;j++){
         pnl_mat_set(path1, j, asset_i, pnl_mat_get(path1,j, asset_i) / (1.0 + fdstep));
         pnl_mat_set(path2, j, asset_i, pnl_mat_get(path2, j, asset_i) / (1.0 - fdstep));
     }
