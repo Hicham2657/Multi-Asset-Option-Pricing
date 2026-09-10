df533b74c81b9a9da0c9fd73e7b858755818f6c2
Hicham Ettahri <hicham.ettahri@grenoble-inp.org>
2026-09-10 12:03:57 +0200

    deltasstdDev alles gut

--- src/MonteCarlo.hpp
+++ src/MonteCarlo.hpp
@@ -14,13 +14,21 @@ class MonteCarlo{
         const Option& _option;
         std::size_t _num_iterations;
         PnlRng* _rng;
+
         PnlMat* _path;
+        PnlMat* _shifted;
+        PnlVect* _sum;
+        PnlVect* _sum_sq;
+
     public:
         MonteCarlo(BlackScholesModel& model, const Option& option, std::size_t num_iterations);
         ~MonteCarlo();
+
         MonteCarlo(const MonteCarlo&) = delete;
         MonteCarlo& operator=(const MonteCarlo&) = delete;
-        PriceAndStdDev price(const PnlMat* past, double t);
-        void delta(const PnlMat* past,double t,double fdstep, PnlVect* deltas, int M);
 
-};
+        PriceAndStdDev price(const PnlMat* past, double t);
+        void delta(const PnlMat* past,double t,double fdstep, PnlVect* deltas, PnlVect* delta_std_dev);
+        PricingResults PriceAndDeltas(const PnlMat* past, double t, double fd_step);
+
+};  
