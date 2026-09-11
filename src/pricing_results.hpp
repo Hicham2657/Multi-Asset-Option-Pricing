637aca49d30395b2207d544e02971d4605afdf1c
Hicham Ettahri <hicham.ettahri@grenoble-inp.org>
2026-09-11 09:15:26 +0200

    pricing results public

--- src/pricing_results.hpp
+++ src/pricing_results.hpp
@@ -4,13 +4,12 @@
 
 class PricingResults
 {
-  private:
+  public:
     double price;
     const PnlVect* delta;
     double priceStdDev;
     const PnlVect* deltaStdDev;
 
-  public:
     PricingResults(double p_price, double p_priceStdDev, const PnlVect* const p_delta, const PnlVect* const p_deltaStdDev);
 
     friend std::ostream& operator<<(std::ostream& stm, const PricingResults& res);
